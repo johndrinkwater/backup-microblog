4abc30d8ec11a30f03185fa0b921b0846ed223f4
John Drinkwater <john@nextraweb.com>
2009-02-03 17:42:18 +0000

    Initial check-in of the programme, it does mostly what I want it to, further edits will: * Get it to support Twitter too. * Be able to construct URLs with a given service + username. * Store more in the dent files(?). * Possibly include a HTML-formatted rendering of the notice. * More links!

--- backup-µblog.py
+++ backup-µblog.py
@@ -8,7 +8,9 @@
 # Licence: GPL v3
 # Licence URL: http://www.gnu.org/licenses/gpl-3.0.html
 #
-import sys,os.path, simplejson as json
+import sys,os.path, simplejson as json, httplib, urllib, urllib2, time, codecs
+
+httplib.HTTPConnection.debuglevel = 1
 
 __version__ = "0.01"
 
@@ -25,7 +27,7 @@ if __name__ == '__main__':
 	username = ""
 	password = ""
 	location = "."
-
+	
 	if len(sys.argv) > 1:
 		URL = sys.argv[1]
 		if URL.find('atom'):
@@ -42,6 +44,66 @@ if __name__ == '__main__':
 		location = os.path.realpath( location )
 		# location doesn’t end in / (or \ win)
 
+		# loop here until end!
+		requestednotices = 33
+		totalnotices = 0
+		requestedpage = 1
+		receivednotices = requestednotices
+
+
+		while receivednotices >= requestednotices:
+			# fetch data
+			noticedata = ""
+			try:
+				params = urllib.urlencode( { "count": requestednotices, "page": requestedpage } )
+				request = urllib2.Request( "%s?%s" % (URL, params) )
+				request.add_header('User-Agent', "Backup µBlog %s %s" % (__version__, ""))
+				# can’t do this without something like: data = gzip.GzipFile(fileobj=f).read()
+				# request.add_header('Accept-encoding', 'gzip')
+				requester = urllib2.build_opener()
+				noticedata = requester.open(request).read()
+			except:
+				print "Failed"
+				sys.exit(-1)
+		
+			# how does this fail?
+			notices = json.loads(noticedata)
+		
+			# loop notices, make files
+			for notice in notices:
+				notice['timestamp'] = time.strptime( notice['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+				notice['filename']  = unicode( os.path.join( location, time.strftime( "%FT%H%M%SZ", notice['timestamp'] ) ), encoding='utf-8', errors='ignore' )
+				print "Writing: %s %s" % ( os.path.basename( notice['filename'] ), notice['text'] )
+				
+				# test if file exists, if so, fail? (we’ve already backed this up)
+				file = codecs.open( notice['filename'], encoding='utf-8', mode='w+' )
+				# geolocation in the future?
+				#file.write( 'Location: %s', notice['user']['location'] )
+				file.write( "Id: %d\n" % notice['id'] )
+				# this is hardcoded, until we add service pulling from dir.laconi.ca
+				file.write( "URI: http://identi.ca/notice/%d\n" % notice['id'] )
+				file.write( "Source: %s\n" % notice['source'] )
+				if notice['in_reply_to_status_id'] is not None:
+					file.write( "In-Reply-To: %s\n" % notice['in_reply_to_status_id'] )
+				if notice['in_reply_to_user_id'] is not None:
+					file.write( "In-Reply-To-User: %s\n" % notice['in_reply_to_user_id'] )
+				file.write( "Truncated: %s\n" % notice['truncated'] )
+				file.write( "Favourited: %s\n" % notice['favorited'] )
+				file.write( "Posted: %s\n" % time.strftime( "%FT%H:%M:%SZ", notice['timestamp'] ) )
+				file.write( "\n" )
+				file.write( "%s\n" % notice['text'] )
+				file.close()
+
+			# increment
+			receivednotices = len(notices)
+			requestedpage = requestedpage + 1
+			totalnotices = totalnotices + receivednotices
+			print "Backed up %d dents so far." % totalnotices
+
+			sys.exit()
+			# sleep here for a little
+			print "Pausing to throttle hitting the server."
+			time.sleep( 10 )
 
 	else:
 		printusage()
